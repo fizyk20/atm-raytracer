4910b601bb110b474ad2daace1722cf239d87a93
Bartłomiej Kamiński <fizyk20@gmail.com>
2019-06-12 19:54:37 +0200

    Improve performance of terrain data lookup

--- src/terrain.rs
+++ src/terrain.rs
@@ -1,24 +1,30 @@
 use dted::{read_dted, DtedData};
+use std::collections::HashMap;
 use std::path::Path;
 
 pub struct Terrain {
-    data: Vec<DtedData>,
+    data: HashMap<(i16, i16), DtedData>,
 }
 
 impl Terrain {
     pub fn new() -> Self {
-        Terrain { data: vec![] }
+        Terrain {
+            data: HashMap::new(),
+        }
     }
 
     pub fn load_dted<P: AsRef<Path>>(&mut self, path: P) {
         let data = read_dted(path).expect("Couldn't read a DTED file");
-        self.data.push(data);
+        let lat = data.min_lat() as i16;
+        let lon = data.min_lon() as i16;
+        let _ = self.data.insert((lat, lon), data);
     }
 
     pub fn get_elev(&self, latitude: f64, longitude: f64) -> Option<f64> {
+        let lat = latitude.floor() as i16;
+        let lon = longitude.floor() as i16;
         self.data
-            .iter()
-            .filter_map(|data| data.get_elev(latitude, longitude))
-            .next()
+            .get(&(lat, lon))
+            .and_then(|data| data.get_elev(latitude, longitude))
     }
 }
