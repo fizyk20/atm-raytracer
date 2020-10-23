d17ba78ef46406691f6cd12ca26718157b3ef1cc
Bartłomiej Kamiński <fizyk20@gmail.com>
2020-10-23 13:09:26 +0200

    Generate terrain normals during tracing

--- src/generate.rs
+++ src/generate.rs
@@ -9,6 +9,7 @@ pub struct ResultPixel {
     pub lon: f64,
     pub distance: f64,
     pub elevation: f64,
+    pub normal: Vector3<f64>,
 }
 
 fn get_ray_elev(params: &Params, y: u16) -> f64 {
@@ -52,48 +53,123 @@ pub fn gen_path_cache(params: &Params, terrain: &Terrain, y: u16) -> Vec<RayStat
     path
 }
 
-pub fn gen_terrain_cache(params: &Params, terrain: &Terrain, x: u16) -> Vec<(f64, f64, f64)> {
+#[derive(Debug, Clone, Copy)]
+pub struct TerrainData {
+    lat: f64,
+    lon: f64,
+    elev: f64,
+    normal: Vector3<f64>,
+}
+
+fn find_normal(shape: &EarthShape, lat: f64, lon: f64, terrain: &Terrain) -> Vector3<f64> {
+    const DIFF: f64 = 15.0;
+
+    let p_north = get_coords_at_dist(shape, (lat, lon), 0.0, DIFF);
+    let p_south = get_coords_at_dist(shape, (lat, lon), 180.0, DIFF);
+    let p_east = get_coords_at_dist(shape, (lat, lon), 90.0, DIFF);
+    let p_west = get_coords_at_dist(shape, (lat, lon), 270.0, DIFF);
+
+    let (dir_north, dir_east, dir_up) = match shape {
+        EarthShape::Flat => (
+            Vector3::new(0.0, 1.0, 0.0),
+            Vector3::new(1.0, 0.0, 0.0),
+            Vector3::new(0.0, 0.0, 1.0),
+        ),
+        EarthShape::Spherical { .. } => {
+            let lat_rad = lat.to_radians();
+            let lon_rad = lon.to_radians();
+
+            let sinlon = lon_rad.sin();
+            let coslon = lon_rad.cos();
+            let sinlat = lat_rad.sin();
+            let coslat = lat_rad.cos();
+
+            let pos = Vector3::new(coslat * coslon, coslat * sinlon, sinlat);
+            // vector tangent to Earth's surface pointing north
+            let dirn = Vector3::new(-sinlat * coslon, -sinlat * sinlon, coslat);
+            // vector tangent to Earth's surface pointing east
+            let dire = Vector3::new(-sinlon, coslon, 0.0);
+
+            (dirn, dire, pos)
+        }
+    };
+
+    let diff_ew = terrain.get_elev(p_east.0, p_east.1).unwrap_or(0.0)
+        - terrain.get_elev(p_west.0, p_west.1).unwrap_or(0.0);
+    let diff_ns = terrain.get_elev(p_north.0, p_north.1).unwrap_or(0.0)
+        - terrain.get_elev(p_south.0, p_south.1).unwrap_or(0.0);
+
+    let vec_ns = 2.0 * DIFF * dir_north + diff_ns * dir_up;
+    let vec_ew = 2.0 * DIFF * dir_east + diff_ew * dir_up;
+
+    let mut normal = vec_ew.cross(&vec_ns);
+    normal.normalize_mut();
+
+    normal
+}
+
+pub fn gen_terrain_cache(params: &Params, terrain: &Terrain, x: u16) -> Vec<TerrainData> {
     let dir = get_ray_dir(params, x);
     let mut distance = 0.0;
 
     let mut result = vec![];
     while distance < params.view.frame.max_distance {
         distance += params.simulation_step;
-        let (lat, lon) = get_coords_at_dist(params, dir, distance);
-        result.push((lat, lon, terrain.get_elev(lat, lon).unwrap_or(0.0)));
+        let (lat, lon) = get_coords_at_dist(
+            &params.env.shape,
+            (
+                params.view.position.latitude,
+                params.view.position.longitude,
+            ),
+            dir,
+            distance,
+        );
+        let normal = find_normal(&params.env.shape, lat, lon, terrain);
+        result.push(TerrainData {
+            lat,
+            lon,
+            elev: terrain.get_elev(lat, lon).unwrap_or(0.0),
+            normal,
+        });
     }
 
     result
 }
 
 pub fn get_single_pixel(
-    terrain_cache: &[(f64, f64, f64)],
+    terrain_cache: &[TerrainData],
     path_cache: &[RayState],
 ) -> Option<ResultPixel> {
-    let (mut lat, mut lon, mut elev) = terrain_cache[0];
+    let TerrainData {
+        mut lat,
+        mut lon,
+        mut elev,
+        mut normal,
+    } = terrain_cache[0];
     let mut dist = 0.0;
     let mut ray_elev = path_cache[0].h;
 
-    for (&(new_lat, new_lon, new_elev), ray_state) in
-        terrain_cache.into_iter().zip(path_cache).skip(1)
-    {
-        if ray_state.h < new_elev {
+    for (terrain_data, ray_state) in terrain_cache.into_iter().zip(path_cache).skip(1) {
+        if ray_state.h < terrain_data.elev {
             let diff1 = ray_elev - elev;
-            let diff2 = ray_state.h - new_elev;
+            let diff2 = ray_state.h - terrain_data.elev;
             let prop = diff1 / (diff1 - diff2);
             let distance = dist + (ray_state.x - dist) * prop;
-            let lat = lat + (new_lat - lat) * prop;
-            let lon = lon + (new_lon - lon) * prop;
+            let lat = lat + (terrain_data.lat - lat) * prop;
+            let lon = lon + (terrain_data.lon - lon) * prop;
+            let normal = normal + (terrain_data.normal - normal) * prop;
             return Some(ResultPixel {
                 lat,
                 lon,
                 distance,
-                elevation: elev + (new_elev - elev) * prop,
+                elevation: elev + (terrain_data.elev - elev) * prop,
+                normal,
             });
         }
-        lat = new_lat;
-        lon = new_lon;
-        elev = new_elev;
+        lat = terrain_data.lat;
+        lon = terrain_data.lon;
+        elev = terrain_data.elev;
+        normal = terrain_data.normal;
         dist = ray_state.x;
         ray_elev = ray_state.h;
     }
@@ -102,23 +178,19 @@ pub fn get_single_pixel(
 
 const DEGREE_DISTANCE: f64 = 111_111.111;
 
-fn get_coords_at_dist(params: &Params, dir: f64, dist: f64) -> (f64, f64) {
-    match params.env.shape {
+fn get_coords_at_dist(shape: &EarthShape, start: (f64, f64), dir: f64, dist: f64) -> (f64, f64) {
+    match shape {
         EarthShape::Flat => {
             let d_lat = dir.to_radians().cos() * dist / DEGREE_DISTANCE;
-            let d_lon = dir.to_radians().sin() * dist
-                / DEGREE_DISTANCE
-                / params.view.position.latitude.to_radians().cos();
-            (
-                params.view.position.latitude + d_lat,
-                params.view.position.longitude + d_lon,
-            )
+            let d_lon =
+                dir.to_radians().sin() * dist / DEGREE_DISTANCE / start.0.to_radians().cos();
+            (start.0 + d_lat, start.1 + d_lon)
         }
         EarthShape::Spherical { radius } => {
             let ang = dist / radius;
 
-            let lat_rad = params.view.position.latitude.to_radians();
-            let lon_rad = params.view.position.longitude.to_radians();
+            let lat_rad = start.0.to_radians();
+            let lon_rad = start.1.to_radians();
 
             let sinlon = lon_rad.sin();
             let coslon = lon_rad.cos();
