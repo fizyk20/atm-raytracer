2b6d3c0d9f871528b134ffa6e973498a59ccc13a
Bartłomiej Kamiński <fizyk20@gmail.com>
2022-03-30 18:30:31 +0200

    Fix get_coords_at_dist for the FlatSpherical model

--- src/utils/earth_model.rs
+++ src/utils/earth_model.rs
@@ -35,24 +35,7 @@ impl EarthModel {
                     Vector3::new(0.0, 0.0, 1.0),
                 )
             }
-            EarthModel::Spherical { .. } => {
-                let lat_rad = lat.to_radians();
-                let lon_rad = lon.to_radians();
-
-                let sinlon = lon_rad.sin();
-                let coslon = lon_rad.cos();
-                let sinlat = lat_rad.sin();
-                let coslat = lat_rad.cos();
-
-                // up direction
-                let dirup = Vector3::new(coslat * coslon, coslat * sinlon, sinlat);
-                // vector tangent to Earth's surface pointing north
-                let dirn = Vector3::new(-sinlat * coslon, -sinlat * sinlon, coslat);
-                // vector tangent to Earth's surface pointing east
-                let dire = Vector3::new(-sinlon, coslon, 0.0);
-
-                (dirn, dire, dirup)
-            }
+            EarthModel::Spherical { .. } => spherical_directions(lat, lon),
         }
     }
 
@@ -107,7 +90,7 @@ impl EarthModel {
             EarthModel::FlatSpherical { radius } | EarthModel::Spherical { radius } => {
                 let ang = dist / radius;
 
-                let (dirn, dire, pos) = self.world_directions(start.0, start.1);
+                let (dirn, dire, pos) = spherical_directions(start.0, start.1);
 
                 // vector tangent to Earth's surface in the given direction
                 let dir_rad = dir.to_radians();
@@ -136,4 +119,23 @@ pub fn spherical_to_cartesian(r: f64, lat: f64, lon: f64) -> Vector3<f64> {
     let y = r * lat.to_radians().cos() * lon.to_radians().sin();
     let z = r * lat.to_radians().sin();
     Vector3::new(x, y, z)
+}
+
+fn spherical_directions(lat: f64, lon: f64) -> (Vector3<f64>, Vector3<f64>, Vector3<f64>) {
+    let lat_rad = lat.to_radians();
+    let lon_rad = lon.to_radians();
+
+    let sinlon = lon_rad.sin();
+    let coslon = lon_rad.cos();
+    let sinlat = lat_rad.sin();
+    let coslat = lat_rad.cos();
+
+    // up direction
+    let dirup = Vector3::new(coslat * coslon, coslat * sinlon, sinlat);
+    // vector tangent to Earth's surface pointing north
+    let dirn = Vector3::new(-sinlat * coslon, -sinlat * sinlon, coslat);
+    // vector tangent to Earth's surface pointing east
+    let dire = Vector3::new(-sinlon, coslon, 0.0);
+
+    (dirn, dire, dirup)
 }
